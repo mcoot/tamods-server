6095f90f4bc9c96abf3468610c617110e191afb6
Joseph Spearritt <joe.spearritt@live.com>
2018-09-02 18:29:58 +1000

    Refactor TCP::Client to use TCP::Connection

--- TCP.h
+++ TCP.h
@@ -21,229 +21,10 @@ using boost::asio::ip::tcp;
 
 namespace TCP {
 
-	// Client represents an asynchronous TCP Client
-	template <typename SizeType>
-	class Client {
-		typedef std::function<void(const json&)> RecvHandlerType;
-	private:
-		boost::system::error_code error_state;
-		bool stopped;
-		boost::asio::deadline_timer deadline;
-		std::vector<SizeType> read_msgsize_buff;
-		std::mutex write_mutex;
-		tcp::socket socket;
-		std::function<void()> onConnectHandler;
-		std::map<short, RecvHandlerType> handlers;
-
-	private:
-
-		void start_connect(tcp::endpoint endpoint) {
-			// Connection deadline of 60 seconds before it is considered a failure
-			deadline.expires_from_now(boost::posix_time::seconds(60));
-
-			socket.async_connect(endpoint, boost::bind(&Client::handle_connect, this, _1));
-		}
-
-		void handle_connect(const boost::system::error_code& conErr) {
-			if (stopped) return;
-
-			if (!socket.is_open()) {
-				// async_connect automatically opens the socket
-				// If the socket is still closed, then timeout occurred
-				// Also check if in an error state
-				stop();
-				error_state = boost::system::error_code(boost::system::errc::connection_aborted, boost::system::generic_category());
-				return;
-			}
-			else if (conErr) {
-				// Connection error
-				stop();
-				error_state = conErr;
-				return;
-			}
-
-			// Successful connection
-
-			// No longer need a deadline
-			deadline.expires_from_now(boost::posix_time::pos_infin);
-
-			// Run the on-connect handler if there is one
-			if (onConnectHandler) {
-				onConnectHandler();
-			}
-
-			// Start the input actor
-			start_read();
-		}
-
-		void start_read() {
-			// Set up asynchronous read for the beginning of a message (first bytes being the message size)
-			boost::asio::async_read(socket, boost::asio::buffer(read_msgsize_buff, sizeof(SizeType)), boost::asio::transfer_exactly(sizeof(SizeType)), boost::bind(&Client::handle_read, this, _1));
-		}
-
-		void handle_read(boost::system::error_code readErr) {
-			if (stopped) return;
-			if (readErr) {
-				error_state = readErr;
-				stop();
-				return;
-			}
-
-			// Already read the message size
-			SizeType msgSize = read_msgsize_buff[0];
-
-			// Synchronously read and extract the message kind
-			short msgKind;
-			size_t syncBytesRead = boost::asio::read(socket, boost::asio::buffer(&msgKind, 2), readErr);
-			if (!readErr && syncBytesRead != 2) {
-				error_state = boost::system::error_code(boost::system::errc::message_size, boost::system::generic_category());
-				stop();
-				return;
-			}
-			if (readErr) {
-				error_state = readErr;
-				stop();
-				return;
-			}
-
-			// Read the actual message
-			std::vector<char> rawMsg(msgSize - 2);
-			syncBytesRead = boost::asio::read(socket, boost::asio::buffer(rawMsg, msgSize - 2), readErr);
-			if (syncBytesRead != msgSize - 2) {
-				error_state = boost::system::error_code(boost::system::errc::message_size, boost::system::generic_category());
-				stop();
-				return;
-			}
-			if (!readErr && readErr) {
-				error_state = readErr;
-				stop();
-				return;
-			}
-			std::string msgString = std::string(rawMsg.begin(), rawMsg.end());
-			// Parse the message into json
-			json msgJson;
-			try {
-				msgJson = json::parse(msgString);
-			}
-			catch (const json::parse_error&) {
-				error_state = boost::system::error_code(boost::system::errc::bad_message, boost::system::generic_category());
-				stop();
-				return;
-			}
-
-			// Call the handler for this kind of message
-			// If there is no handler, ignore the message
-			auto& it = handlers.find(msgKind);
-			if (it != handlers.end()) {
-				it->second(msgJson);
-			}
-
-			// Begin the next asynchronous read
-			start_read();
-		}
-
-		void write(short msgKind, const json& msgBody) {
-			if (stopped) return;
-
-			// Serialise message
-			std::string msgString = msgBody.dump();
-			SizeType msgSize = (SizeType)msgString.length() + sizeof(msgKind);
-
-			// Construct the bytes to send
-			std::ostringstream out_stream;
-			out_stream.write(reinterpret_cast<char*>(&msgSize), sizeof(msgSize));
-			out_stream.write(reinterpret_cast<char*>(&msgKind), sizeof(msgKind));
-			out_stream << msgString;
-
-			// Message writing is synchronous and serialised to avoid a data race
-			boost::system::error_code writeErr;
-			
-			size_t bytesWritten;
-			{
-				std::lock_guard<std::mutex> lock(write_mutex);
-				bytesWritten = boost::asio::write(socket, boost::asio::buffer(out_stream.str(), out_stream.str().length()), writeErr);
-			}
-			if (!writeErr && bytesWritten != msgSize + 2) {
-				writeErr.assign(boost::system::errc::message_size, boost::system::generic_category());
-			}
-			if (writeErr) {
-				error_state = writeErr;
-				stop();
-			}
-		}
-
-		void check_deadline() {
-			if (stopped) return;
-
-			// Check if the deadline has passed
-			if (deadline.expires_at() <= boost::asio::deadline_timer::traits_type::now()) {
-				// Kill the connection, anything outstanding will be cancelled
-				socket.close();
-				// Set infinite expiry since the deadline is complete
-				deadline.expires_at(boost::posix_time::pos_infin);
-			}
-
-			// Put the actor back to sleep.
-			deadline.async_wait(boost::bind(&Client::check_deadline, this));
-		}
-
-	public:
-		Client(boost::asio::io_service& io_service) :
-			socket(io_service),
-			read_msgsize_buff(1),
-			write_mutex(),
-			deadline(io_service),
-			error_state(),
-			handlers(),
-			stopped(false) {
-			static_assert(std::is_integral<SizeType>::value, "SizeType type parameter must be an integral type");
-		}
-
-		void add_handler(short msgKind, RecvHandlerType handler) {
-			handlers[msgKind] = handler;
-		}
-
-		void send(short msgKind, const json& msgBody) {
-			write(msgKind, msgBody);
-		}
-
-		void start(std::string host, int port, std::function<void()> connect_handler = NULL) {
-			stopped = false;
-			onConnectHandler = connect_handler;
-			boost::asio::ip::address addr = boost::asio::ip::address::from_string(host, error_state);
-			if (error_state) {
-				return;
-			}
-			tcp::endpoint endpoint(addr, port);
-
-			start_connect(endpoint);
-
-			// Start the deadline actor
-			// This doesn't actually set a specific deadline, that is set elsewhere
-			deadline.async_wait(boost::bind(&Client::check_deadline, this));
-		}
-
-		void stop() {
-			Logger::debug("stopping client... error code is %d: %s", error_state.value(), error_state.message());
-			stopped = true;
-			socket.close();
-			deadline.cancel();
-		}
-
-		boost::system::error_code get_error_state() {
-			return error_state;
-		}
-
-		bool is_stopped() {
-			return stopped;
-		}
-
-	};
-
 	// Connection represents an ongoing TCP connection with a client
 	template <typename SizeType>
 	class Connection : public std::enable_shared_from_this<Connection<SizeType> > {
-	private:
+	protected:
 		typedef std::function<void(const json&)> RecvHandlerType;
 		boost::system::error_code error_state;
 		bool stopped;
@@ -252,9 +33,9 @@ namespace TCP {
 		tcp::socket socket;
 		std::function<void(boost::system::error_code&)> onStopHandler;
 		std::map<short, RecvHandlerType> handlers;
-	private:
+	protected:
 
-		void handle_read() {
+		void handle_read(boost::system::error_code readErr) {
 			if (stopped) return;
 			if (readErr) {
 				error_state = readErr;
@@ -349,8 +130,8 @@ namespace TCP {
 		}
 
 	public:
-		Connection(boost::asio::io_service& ios) : 
-			socket(io_service),
+		Connection(boost::asio::io_service& ios) :
+			socket(ios),
 			read_msgsize_buff(1),
 			write_mutex(),
 			error_state(),
@@ -371,15 +152,17 @@ namespace TCP {
 			write(msgKind, msgBody);
 		}
 
-		void start(std::function<void()> connect_handler = NULL, std::function<void(boost::system::error_code&)> stop_handler = NULL) {
+		virtual void start(std::function<void()> connect_handler = NULL, std::function<void(boost::system::error_code&)> stop_handler = NULL) {
 			stopped = false;
 			onStopHandler = stop_handler;
-			boost::asio::async_read(socket, 
-				boost::asio::buffer(read_msgsize_buff, sizeof(SizeType)), 
-				boost::asio::transfer_exactly(sizeof(SizeType)), 
+			boost::asio::async_read(socket,
+				boost::asio::buffer(read_msgsize_buff, sizeof(SizeType)),
+				boost::asio::transfer_exactly(sizeof(SizeType)),
 				boost::bind(&Connection::handle_read, this, _1));
 
-			connect_handler();
+			if (connect_handler) {
+				connect_handler();
+			}
 		}
 
 		void stop() {
@@ -400,6 +183,56 @@ namespace TCP {
 			return stopped;
 		}
 
+	};
+
+	// Client represents an asynchronous TCP Client
+	template <typename SizeType>
+	class Client : public Connection<SizeType> {
+		typedef std::function<void(const json&)> RecvHandlerType;
+	private:
+		std::function<void()> onConnectHandler;
+	private:
+
+		void handle_connect(const boost::system::error_code& conErr) {
+			if (stopped) return;
+
+			if (!socket.is_open()) {
+				// async_connect automatically opens the socket
+				// If the socket is still closed, then timeout occurred
+				// Also check if in an error state
+				stop();
+				error_state = boost::system::error_code(boost::system::errc::connection_aborted, boost::system::generic_category());
+				return;
+			}
+			else if (conErr) {
+				// Connection error
+				stop();
+				error_state = conErr;
+				return;
+			}
+
+			// Successful connection
+			this->Connection<SizeType>::start(onConnectHandler, onStopHandler);
+		}
+
+	public:
+		Client(boost::asio::io_service& ios) : 
+			Connection(ios) {
+			static_assert(std::is_integral<SizeType>::value, "SizeType type parameter must be an integral type");
+		}
+
+		virtual void start(std::string host, int port, std::function<void()> connect_handler = NULL, std::function<void(boost::system::error_code&)> stop_handler = NULL) {
+			stopped = false;
+			onConnectHandler = connect_handler;
+			onStopHandler = stop_handler;
+			boost::asio::ip::address addr = boost::asio::ip::address::from_string(host, error_state);
+			if (error_state) {
+				return;
+			}
+			tcp::endpoint endpoint(addr, port);
+
+			socket.async_connect(endpoint, boost::bind(&Client::handle_connect, this, _1));
+		}
 	};
 
 	template <typename SizeType>
