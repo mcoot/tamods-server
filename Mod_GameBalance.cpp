9875494872db935653cc141aec64eb09ad1765eb
JosephSpearritt <joe.spearritt@live.com>
2018-09-22 14:05:28 +1000

    Add getters for GameBalance Properties

--- Mod_GameBalance.cpp
+++ Mod_GameBalance.cpp
@@ -8,6 +8,10 @@ namespace GameBalance {
 	bool Property::apply(PropValue value, UObject* obj) {
 		return (value.type == type && applier(value, obj));
 	}
+
+	bool Property::get(UObject* obj, PropValue& ret) {
+		return getter(obj, ret);
+	}
 }
 
 namespace DCServer {
